2207ccc5c87aec4b443246277caab5995cabef35
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-11-27 14:05:10 -0800

    Updates

---
+++ precache-manifest.c290d4754ff2703925532bc93df02726.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "054e4c8ae68be6ea5321c9cd4d645f05",
+    "url": "/mapping-democracy/static/media/senate-icon.054e4c8a.svg"
+  },
+  {
+    "revision": "2032e192b740cc2def909c1bda5c41c5",
+    "url": "/mapping-democracy/static/media/ineligible-icon.2032e192.svg"
+  },
+  {
+    "revision": "1d489b77e9e13731532cc6e64fc13949",
+    "url": "/mapping-democracy/static/media/house-icon.1d489b77.svg"
+  },
+  {
+    "revision": "56ce560b5bcfd926e220df255b8635a7",
+    "url": "/mapping-democracy/static/media/US-outline.56ce560b.svg"
+  },
+  {
+    "revision": "b770a094858b1f3a9a33779f6324c948",
+    "url": "/mapping-democracy/static/media/Blank_US_Map.b770a094.svg"
+  },
+  {
+    "revision": "95ea65063fb10b524b1a",
+    "url": "/mapping-democracy/static/js/runtime~main.95ea6506.js"
+  },
+  {
+    "revision": "099197de6a93671f5b07",
+    "url": "/mapping-democracy/static/js/main.099197de.chunk.js"
+  },
+  {
+    "revision": "16ffa488330a054c9748",
+    "url": "/mapping-democracy/static/js/1.16ffa488.chunk.js"
+  },
+  {
+    "revision": "099197de6a93671f5b07",
+    "url": "/mapping-democracy/static/css/main.c55b8c01.chunk.css"
+  },
+  {
+    "revision": "66c29b3c0f3a8875bc15dbba1035d38d",
+    "url": "/mapping-democracy/index.html"
+  }
+];
