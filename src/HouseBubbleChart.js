6d9455aefd0aca3e412afd7efbe38a0dfb53668c
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-12-03 22:07:53 -0800

    finished house bubble chart

--- src/HouseBubbleChart.js
+++ src/HouseBubbleChart.js
@@ -60,38 +60,47 @@ class HouseBubbleChart extends React.Component {
 
 
 		return(
-			<div className="section">
+			<div >
 
 				<svg width="350" height="500"> 
 					<circle cx="60" cy="60" r={mtr}  fill="DeepPink"></circle>
-					<text x="55" y="55" fill="white">MT</text>
-					<text x="45" y="68" fill="white">{((mtp/1000)/mtreps).toFixed()}K</text>
+					<text x="50" y="54" fill="white">MT</text>
+					<text x="40" y="68" fill="white">{((mtp/1000)/mtreps).toFixed()}K</text>
+					<text x="56" y="82" fill="white">{mtreps}</text>
 					<circle cx="170" cy="60" r={rir}  fill="DeepSkyBlue"></circle>
-					<text x="165" y="63" fill="white">RI</text>
-					<text x="155" y="75" fill="white">{((rip/1000)/rireps).toFixed()}K</text>
+					<text x="160" y="54" fill="white">RI</text>
+					<text x="150" y="68" fill="white">{((rip/1000)/rireps).toFixed()}K</text>
+					<text x="165" y="82" fill="white">{rireps}</text>
 					<circle cx="290" cy="60" r={dlr}  fill="DeepSkyBlue"></circle>
-					<text x="285" y="60" fill="white">DL</text>
-					<text x="275" y="73" fill="white">{((dlp/1000)/dlreps).toFixed()}K</text>
+					<text x="280" y="54" fill="white">DL</text>
+					<text x="270" y="68" fill="white">{((dlp/1000)/dlreps).toFixed()}K</text>
+					<text x="285" y="82" fill="white">{dlreps}</text>
 
-					<circle cx="57" cy="170" r={wyr}  fill="DeepSkyBlue"></circle>
-					<text x="57" y="170" fill="white">WY</text>
-					<text x="47" y="183" fill="white">{((wyp/1000)/wyreps).toFixed()}K</text>
-					<circle cx="170" cy="170" r={kyr}  fill="DeepSkyBlue"></circle>
-					<text x="170" y="160" fill="white">KY</text>
-					<text x="160" y="173" fill="white">{((kyp/1000)/kyreps).toFixed()}K</text>
-					<circle cx="295" cy="175" r={gar}  fill="DeepPink"></circle>
-					<text x="295" y="170" fill="white">GA</text>
-					<text x="285" y="183" fill="white">{((gap/1000)/gareps).toFixed()}K</text>
+					<circle cx="60" cy="170" r={wyr}  fill="DeepPink"></circle>
+					<text x="47" y="165" fill="white">WY</text>
+					<text x="40" y="178" fill="white">{((wyp/1000)/wyreps).toFixed()}K</text>
+					<text x="56" y="191" fill="white">{wyreps}</text>
+					<circle cx="170" cy="170" r={kyr}  fill="Indigo"></circle>
+					<text x="158" y="160" fill="white">KY</text>
+					<text x="150" y="173" fill="white">{((kyp/1000)/kyreps).toFixed()}K</text>
+					<text x="162" y="186" fill="white">{kyreps}</text>
+					<circle cx="295" cy="170" r={gar}  fill="Indigo"></circle>
+					<text x="285" y="160" fill="white">GA</text>
+					<text x="275" y="174" fill="white">{((gap/1000)/gareps).toFixed()}K</text>
+					<text x="287" y="187" fill="white">{gareps}</text>
 
 					<circle cx="60" cy="280" r={par}  fill="Indigo"></circle>
-					<text x="58" y="285" fill="white">PA</text>
-					<text x="48" y="298" fill="white">{((pap/1000)/pareps).toFixed()}K</text>
-					<circle cx="169" cy="286" r={sdr}  fill="Indigo"></circle>
-					<text x="160" y="283" fill="white">SD</text>
-					<text x="150" y="295" fill="white">{((sdp/1000)/sdreps).toFixed()}K</text>
-					<circle cx="295" cy="280" r={akr}  fill="DeepSkyBlue"></circle>
-					<text x="295" y="280" fill="white">AK</text>
-					<text x="285" y="293" fill="white">{((akp/1000)/akreps).toFixed()}K</text>
+					<text x="50" y="275" fill="white">PA</text>
+					<text x="42" y="288" fill="white">{((pap/1000)/pareps).toFixed()}K</text>
+					<text x="51" y="301" fill="white">{pareps}</text>
+					<circle cx="169" cy="280" r={sdr}  fill="DeepPink"></circle>
+					<text x="158" y="275" fill="white">SD</text>
+					<text x="150" y="288" fill="white">{((sdp/1000)/sdreps).toFixed()}K</text>
+					<text x="164" y="302" fill="white">{sdreps}</text>
+					<circle cx="295" cy="280" r={akr}  fill="DeepPink"></circle>
+					<text x="282" y="272" fill="white">AK</text>
+					<text x="275" y="286" fill="white">{((akp/1000)/akreps).toFixed()}K</text>
+					<text x="285" y="299" fill="white">{akreps}</text>
 
 
 
--- src/TestSlider.js
+++ src/TestSlider.js
@@ -1,7 +1,7 @@
 
 import React, {Component} from 'react';
 import HouseBubbleChart from './HouseBubbleChart.js';
-
+import './App.css';
 
 class TestSlider extends Component {
 
@@ -25,15 +25,15 @@ class TestSlider extends Component {
 			<h3> Very small states are still over-represented, while states that are just below the population threshhold for an additional representative are under-represented. </h3>
 			<h3> Some activists have called for increasing the number of representatives in the House in order to lessen this disparity. Within reason, the "optimal" number of seats is 930, with roughly 330,000 people per representative.</h3>
 
-			<h3> This selection of states shows how people per representative changes based on the number of seats in the House: the larger the bubble, the more people per representative. </h3>
-        <div style={{ marginTop: "20px", marginBottom: "20px" }}>
+			<h3> Adjust the slider to see how people per representative changes based on the number of seats in the House: the larger the bubble, the more people per representative. </h3>
+        <div style={{ marginTop: "2px", marginBottom: "20px" }}>
           <input
-            id={"A"}
+          class="slider"
+            id={"houseSlider"}
             type="range"
             defaultValue="435"
             min="435"
             max="1024"
-            className="slider"
             onChange={this.handleSliderChange.bind(this)}
             step="5"
           />
@@ -47,13 +47,9 @@ class TestSlider extends Component {
 
   handleSliderChange(event) {
     console.log(event.target.value, " ", event.target.id);
-    var id = event.target.id;
     var value = event.target.value;
-    const slidersWeights = this.state.slidersWeights;
-    slidersWeights[id] = parseInt(value);
     var sliderAmt = event.target.value;
-    const slidersLabels = this.state.slidersLabels;
-    this.setState({ slidersWeights, sliderAmt });
+    this.setState({ sliderAmt });
   }
 }
 
